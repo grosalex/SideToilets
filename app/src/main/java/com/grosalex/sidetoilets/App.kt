c2f8bff58e2e2ecbb699aa3c1123a8b0bc3763d3
grosalex <alexbruneau93@gmail.com>
2019-07-13 08:59:51 +0200

    init application override

--- app/src/main/java/com/grosalex/sidetoilets/App.kt
+++ app/src/main/java/com/grosalex/sidetoilets/App.kt
@@ -1,9 +1,26 @@
 package com.grosalex.sidetoilets
 
 import android.app.Application
+import com.grosalex.sidetoilets.api.RatpService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class App: Application() {
+class App : Application() {
+    lateinit var service: RatpService
     override fun onCreate() {
         super.onCreate()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(RatpService.ROOT)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        service = retrofit.create<RatpService>(RatpService::class.java)
+        app = this
+    }
+
+    companion object {
+        lateinit var app: App
+
+        fun get(): App = app
     }
 }
